c6e0b31105d0b16b8315c199482b82d3151fedeb
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2026-04-06 21:24:58 +0200

    The thing

--- src/main/java/com/fruityspikes/whaleborne/server/registries/WBSoundRegistry.java
+++ src/main/java/com/fruityspikes/whaleborne/server/registries/WBSoundRegistry.java
@@ -18,43 +18,17 @@ public class WBSoundRegistry {
     public static final DeferredRegister<SoundEvent> SOUND_EVENTS =
             DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, Whaleborne.MODID);
 
-    public static final Supplier<SoundEvent> ORGAN = SOUND_EVENTS.register(
-            "block.barnacle.organ",
-            () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(Whaleborne.MODID, "block.barnacle.organ"))
-    );
+    public static final Supplier<SoundEvent> ORGAN = register("block.barnacle.organ");
+    public static final Supplier<SoundEvent> HULLBACK_DEATH = register("entity.hullback.death");
+    public static final Supplier<SoundEvent> HULLBACK_HURT = register("entity.hullback.hurt");
+    public static final Supplier<SoundEvent> HULLBACK_TAME = register("entity.hullback.tame");
+    public static final Supplier<SoundEvent> HULLBACK_SWIM = register("entity.hullback.swim");
+    public static final Supplier<SoundEvent> HULLBACK_BREATHE = register("entity.hullback.breathe");
+    public static final Supplier<SoundEvent> HULLBACK_AMBIENT = register("entity.hullback.ambient");
+    public static final Supplier<SoundEvent> HULLBACK_HAPPY = register("entity.hullback.happy");
+    public static final Supplier<SoundEvent> HULLBACK_MAD = register("entity.hullback.mad");
 
-    public static final Supplier<SoundEvent> HULLBACK_DEATH = SOUND_EVENTS.register(
-            "entity.hullback.death",
-            () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(Whaleborne.MODID, "entity.hullback.death"))
-    );
-    public static final Supplier<SoundEvent> HULLBACK_HURT = SOUND_EVENTS.register(
-            "entity.hullback.hurt",
-            () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(Whaleborne.MODID, "entity.hullback.hurt"))
-    );
-    public static final Supplier<SoundEvent> HULLBACK_TAME = SOUND_EVENTS.register(
-            "entity.hullback.tame",
-            () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(Whaleborne.MODID, "entity.hullback.tame"))
-    );
-    public static final Supplier<SoundEvent> HULLBACK_SWIM = SOUND_EVENTS.register(
-            "entity.hullback.swim",
-            () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(Whaleborne.MODID, "entity.hullback.swim"))
-    );
-    public static final Supplier<SoundEvent> HULLBACK_BREATHE = SOUND_EVENTS.register(
-            "entity.hullback.breathe",
-            () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(Whaleborne.MODID, "entity.hullback.breathe"))
-    );
-    public static final Supplier<SoundEvent> HULLBACK_AMBIENT = SOUND_EVENTS.register(
-            "entity.hullback.ambient",
-            () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(Whaleborne.MODID, "entity.hullback.ambient"))
-    );
-    public static final Supplier<SoundEvent> HULLBACK_HAPPY = SOUND_EVENTS.register(
-            "entity.hullback.happy",
-            () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(Whaleborne.MODID, "entity.hullback.happy"))
-    );
-    public static final Supplier<SoundEvent> HULLBACK_MAD = SOUND_EVENTS.register(
-            "entity.hullback.mad",
-            () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(Whaleborne.MODID, "entity.hullback.mad"))
-    );
+    public static final Supplier<SoundEvent> MUSIC_DISC_THE_PLANK = register("music_disc.the_plank");
 
     public static final SoundType BARNACLE_BLOCK = register("barnacle_block", 1, 1.35F);
 
